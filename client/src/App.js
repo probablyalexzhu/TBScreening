b91ac01252f7fe0a31db80a9c7787b99a4ffb13b
Alex <a3zhu@uwaterloo.ca>
2024-03-30 18:18:25 -0400

    update: voice progress

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,15 @@ import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 import {useState, useEffect} from 'react'
 
+// speech recognition setup
+const SpeechRecognition =
+window.SpeechRecognition || window.webkitSpeechRecognition
+const mic = new SpeechRecognition()
+
+mic.continuous = true
+mic.interimResults = true
+mic.lang = 'en-US'
+
 function App() {
   // store data from backend in data
   const [data, setData] = useState([{}]);
@@ -28,8 +37,76 @@ function App() {
       });
   }, []);
 
+  // speech recognition functions
+  const [isListening, setIsListening] = useState(false)
+  const [note, setNote] = useState(null)
+  const [savedNotes, setSavedNotes] = useState([])
+
+  useEffect(() => {
+    handleListen()
+  }, [isListening])
+
+  const handleListen = () => {
+    if (isListening) {
+      mic.start()
+      mic.onend = () => {
+        console.log('continue..')
+        mic.start()
+      }
+    } else {
+      mic.stop()
+      mic.onend = () => {
+        console.log('Stopped Mic on Click')
+      }
+    }
+    mic.onstart = () => {
+      console.log('Mics on')
+    }
+
+    mic.onresult = event => {
+      const transcript = Array.from(event.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+      console.log(transcript)
+      setNote(transcript)
+      mic.onerror = event => {
+        console.log(event.error)
+      }
+    }
+  }
+
+  const handleSaveNote = () => {
+    setSavedNotes([...savedNotes, note])
+    setNote('')
+  }
+
   return (
-    <ChakraProvider theme={theme}>
+    // <ChakraProvider theme={theme}>
+    <div>
+      <>
+        <h1>Voice Notes</h1>
+        <div className="container">
+          <div className="box">
+            <h2>Current Note</h2>
+            {isListening ? <span>🎙️</span> : <span>🛑🎙️</span>}
+            <button onClick={handleSaveNote} disabled={!note}>
+              Save Note
+            </button>
+            <button onClick={() => setIsListening(prevState => !prevState)}>
+              Start/Stop
+            </button>
+            <p>{note}</p>
+          </div>
+          <div className="box">
+            <h2>Notes</h2>
+            {savedNotes.map(n => (
+              <p key={n}>{n}</p>
+            ))}
+          </div>
+        </div>
+      </>
+      
       <div>
         {typeof data.members === 'undefined' ? (
           <p>Loading...</p>
@@ -37,8 +114,9 @@ function App() {
           data.members.map((member, i) => <p key={i}>{member}</p>)
         )}
       </div>
-    </ChakraProvider>
-  );
+    </div>
+    // </ChakraProvider>
+  )
 }
 
 export default App;

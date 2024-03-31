0a9e0d20e9e411d0764a80a4e5ce98aaca8b9c16
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-03-30 21:39:52 -0400

    cleaned up, updated todo

--- flask-backend/report-generator.py
+++ flask-backend/report-generator.py
@@ -10,20 +10,15 @@ exposure = "Exposure blah blah blah"
 ai_comments = "AI Comments blah blah blah"
 risk_level_summary = "Risk Level Summary blah blah blah"
 
-today_date = datetime.today().strftime("%d %b, %Y")
-month = datetime.today().strftime("%B")
-
 context = {'name': name, 'date_completed': date_completed, 'general': general, 'symptoms': symptoms, 'exposure': exposure, 'ai_comments': ai_comments, 'risk_level_summary': risk_level_summary}
 
 template_loader = jinja2.FileSystemLoader('./')
 template_env = jinja2.Environment(loader=template_loader)
 
 html_template = 'doctor_report.html'
-# html_template = 'NOIMAGES_doctor_report.html'
 template = template_env.get_template(html_template)
 output_text = template.render(context)
 
 config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
 output_pdf = 'doctor_report.pdf'
-# output_pdf = 'NOIMAGES_doctor_report.pdf'
 pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": ""})

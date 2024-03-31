18f864883079db95e04bd858cf3a918db850db2c
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-03-30 20:39:04 -0400

    temp push

--- flask-backend/report-generator.py
+++ flask-backend/report-generator.py
@@ -4,6 +4,12 @@ from datetime import datetime
 
 client_name = "Frank Ocean 2"
 date_completed = "2021/09/01"
+general = "General blah blah blah"
+symptoms = "Symptoms blah blah blah"
+exposure = "Exposure blah blah blah"
+ai_comments = "AI Comments blah blah blah"
+risk_level_summary = "Risk Level Summary blah blah blah"
+
 
 item1 = "TV"
 item2 = "Couch"
@@ -17,11 +23,13 @@ total = subtotal1 + subtotal2 + subtotal3
 today_date = datetime.today().strftime("%d %b, %Y")
 month = datetime.today().strftime("%B")
 
-context = {'client_name': client_name, 'today_date': today_date, 'date_completed': date_completed, 'total': f'${total:.2f}', 'month': month,
-           'item1': item1, 'subtotal1': f'${subtotal1:.2f}',
-           'item2': item2, 'subtotal2': f'${subtotal2:.2f}',
-           'item3': item3, 'subtotal3': f'${subtotal3:.2f}'
-           }
+# context = {'client_name': client_name, 'today_date': today_date, 'date_completed': date_completed, 'total': f'${total:.2f}', 'month': month,
+#            'item1': item1, 'subtotal1': f'${subtotal1:.2f}',
+#            'item2': item2, 'subtotal2': f'${subtotal2:.2f}',
+#            'item3': item3, 'subtotal3': f'${subtotal3:.2f}'
+#            }
+
+context = {'client_name': client_name, 'date_completed': date_completed, 'general': general, 'symptoms': symptoms, 'exposure': exposure, 'ai_comments': ai_comments, 'risk_level_summary': risk_level_summary}
 
 template_loader = jinja2.FileSystemLoader('./')
 template_env = jinja2.Environment(loader=template_loader)

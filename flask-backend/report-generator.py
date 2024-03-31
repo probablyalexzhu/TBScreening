5bdd01ce21e45ac8a83f3293be4d560f4170653c
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-03-31 06:32:38 -0400

    Pdf works with full filepath.. but its hardcoded to my computer hm

--- flask-backend/report-generator.py
+++ flask-backend/report-generator.py
@@ -24,21 +24,42 @@ from datetime import datetime
 # output_pdf = 'doctor_report.pdf'
 # pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": ""})
 
-def generate_report(name, general, symptoms, exposure, ai_comments, risk_level_summary):
-    date_completed = datetime.now().strftime("%Y-%m-%d")
-    context = {'name': name, 'date_completed': date_completed, 'general': general, 'symptoms': symptoms, 'exposure': exposure, 'ai_comments': ai_comments, 'risk_level_summary': risk_level_summary}
+# def generate_report_old(name, general, symptoms, exposure, ai_comments, risk_level_summary):
+#     date_completed = datetime.now().strftime("%Y-%m-%d")
+#     context = {'name': name, 'date_completed': date_completed, 'general': general, 'symptoms': symptoms, 'exposure': exposure, 'ai_comments': ai_comments, 'risk_level_summary': risk_level_summary}
+
+#     template_loader = jinja2.FileSystemLoader('./')
+#     template_env = jinja2.Environment(loader=template_loader)
+
+#     html_template = 'doctor_report_template.html'
+#     template = template_env.get_template(html_template)
+#     output_text = template.render(context)
+
+#     config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+#     output_pdf = 'doctor_report_old.pdf'
+#     pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": ""})
+
+#     return output_pdf
+
+# generate_report("Frank Ocean 2", "General blah blah blah blah", "Symptoms blah blah blah", "Exposure blah blah blah", "AI Comments blah blah blah", "Risk Level Summary blah blah blah")
+
+def generate_report(name, age, gender, location, summary, technical):
+    date = datetime.now().strftime("%Y-%m-%d")
+    context = {'name': name, 'date': date, 'age': age, 'gender': gender, 'location': location, 'summary': summary, 'technical': technical}
 
     template_loader = jinja2.FileSystemLoader('./')
     template_env = jinja2.Environment(loader=template_loader)
 
-    html_template = 'doctor_report_template.html'
+    html_template = 'TuberculosisAIPreScreener.html'
     template = template_env.get_template(html_template)
     output_text = template.render(context)
 
     config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
-    output_pdf = 'doctor_report_2.pdf'
-    pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": ""})
+    output_pdf = 'doctor_report_new.pdf'
+    # pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": ""})
+    pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": True})
 
     return output_pdf
 
-generate_report("Frank Ocean 2", "General blah blah blah blah", "Symptoms blah blah blah", "Exposure blah blah blah", "AI Comments blah blah blah", "Risk Level Summary blah blah blah")
+
+generate_report("Anoop Rehman", "67", "Male", "Dharavi", "Summary blah blah blah", "Technical blah blah blah")

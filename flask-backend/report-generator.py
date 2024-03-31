005504ec91b1d495432d46c94e39866d22641625
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-03-30 20:42:16 -0400

    Okkk the original fields all work now!

--- flask-backend/report-generator.py
+++ flask-backend/report-generator.py
@@ -2,23 +2,13 @@ import jinja2
 import pdfkit
 from datetime import datetime
 
-client_name = "Frank Ocean 2"
+name = "Frank Ocean"
 date_completed = "2021/09/01"
 general = "General blah blah blah"
 symptoms = "Symptoms blah blah blah"
 exposure = "Exposure blah blah blah"
 ai_comments = "AI Comments blah blah blah"
 risk_level_summary = "Risk Level Summary blah blah blah"
-
-
-item1 = "TV"
-item2 = "Couch"
-item3 = "Washing Machine"
-
-subtotal1 = 499
-subtotal2 = 399
-subtotal3 = 129
-total = subtotal1 + subtotal2 + subtotal3
 
 today_date = datetime.today().strftime("%d %b, %Y")
 month = datetime.today().strftime("%B")
@@ -29,17 +19,15 @@ month = datetime.today().strftime("%B")
 #            'item3': item3, 'subtotal3': f'${subtotal3:.2f}'
 #            }
 
-context = {'client_name': client_name, 'date_completed': date_completed, 'general': general, 'symptoms': symptoms, 'exposure': exposure, 'ai_comments': ai_comments, 'risk_level_summary': risk_level_summary}
+context = {'name': name, 'date_completed': date_completed, 'general': general, 'symptoms': symptoms, 'exposure': exposure, 'ai_comments': ai_comments, 'risk_level_summary': risk_level_summary}
 
 template_loader = jinja2.FileSystemLoader('./')
 template_env = jinja2.Environment(loader=template_loader)
 
-# html_template = 'invoice.html'
 html_template = 'doctor_report.html'
 template = template_env.get_template(html_template)
 output_text = template.render(context)
 
 config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
 output_pdf = 'doctor_report.pdf'
-# pdfkit.from_string(output_text, output_pdf, configuration=config, css='invoice.css')
 pdfkit.from_string(output_text, output_pdf, configuration=config)

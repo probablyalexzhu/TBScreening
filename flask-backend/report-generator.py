4953584eb83d85b6456146e9440d941ce268930c
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-03-31 07:51:15 -0400

    FINAL REPORT.

--- flask-backend/report-generator.py
+++ flask-backend/report-generator.py
@@ -2,47 +2,6 @@ import jinja2
 import pdfkit
 from datetime import datetime
 
-# date_completed = datetime.now().strftime("%Y-%m-%d")
-
-# name = "Frank Ocean"
-# general = "General blah blah blah"
-# symptoms = "Symptoms blah blah blah"
-# exposure = "Exposure blah blah blah"
-# ai_comments = "AI Comments blah blah blah"
-# risk_level_summary = "Risk Level Summary blah blah blah"
-
-# context = {'name': name, 'date_completed': date_completed, 'general': general, 'symptoms': symptoms, 'exposure': exposure, 'ai_comments': ai_comments, 'risk_level_summary': risk_level_summary}
-
-# template_loader = jinja2.FileSystemLoader('./')
-# template_env = jinja2.Environment(loader=template_loader)
-
-# html_template = 'doctor_report.html'
-# template = template_env.get_template(html_template)
-# output_text = template.render(context)
-
-# config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
-# output_pdf = 'doctor_report.pdf'
-# pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": ""})
-
-# def generate_report_old(name, general, symptoms, exposure, ai_comments, risk_level_summary):
-#     date_completed = datetime.now().strftime("%Y-%m-%d")
-#     context = {'name': name, 'date_completed': date_completed, 'general': general, 'symptoms': symptoms, 'exposure': exposure, 'ai_comments': ai_comments, 'risk_level_summary': risk_level_summary}
-
-#     template_loader = jinja2.FileSystemLoader('./')
-#     template_env = jinja2.Environment(loader=template_loader)
-
-#     html_template = 'doctor_report_template.html'
-#     template = template_env.get_template(html_template)
-#     output_text = template.render(context)
-
-#     config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
-#     output_pdf = 'doctor_report_old.pdf'
-#     pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": ""})
-
-#     return output_pdf
-
-# generate_report("Frank Ocean 2", "General blah blah blah blah", "Symptoms blah blah blah", "Exposure blah blah blah", "AI Comments blah blah blah", "Risk Level Summary blah blah blah")
-
 def generate_report(name, age, gender, location, summary, technical):
     date = datetime.now().strftime("%Y-%m-%d")
     context = {'name': name, 'date': date, 'age': age, 'gender': gender, 'location': location, 'summary': summary, 'technical': technical}
@@ -50,16 +9,16 @@ def generate_report(name, age, gender, location, summary, technical):
     template_loader = jinja2.FileSystemLoader('./')
     template_env = jinja2.Environment(loader=template_loader)
 
-    html_template = 'TuberculosisAIPreScreener.html'
+    html_template = 'FINAL_TuberculosisAIPreScreener.html'
     template = template_env.get_template(html_template)
     output_text = template.render(context)
 
     config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
-    output_pdf = 'doctor_report_new.pdf'
+    output_pdf = 'FINAL_doctor_report.pdf'
     # pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": ""})
-    pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": True})
+    pdfkit.from_string(output_text, output_pdf, configuration=config, options={"enable-local-file-access": True, "page-size": "Letter"})
 
     return output_pdf
 
 
-generate_report("Anoop Rehman", "67", "Male", "Dharavi", "Summary blah blah blah", "Technical blah blah blah")
+generate_report("Anoop Rehman", "67", "Male", "Dharavi", "SUMMARY Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh viverra non semper suscipit posuere a pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", "Technical Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh viverra non semper suscipit posuere a pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.")
